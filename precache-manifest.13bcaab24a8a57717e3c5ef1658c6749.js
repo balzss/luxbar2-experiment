b95f56fb6cf878446f51be8ad49b64512eca8fbb
balazs.saros <balazs.saros@gmail.com>
2019-02-09 15:28:10 +0100

    Updates

---
+++ precache-manifest.13bcaab24a8a57717e3c5ef1658c6749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dbf3eaf64fe8972ac9e",
+    "url": "/luxbar2-experiment/static/js/runtime~main.8dbf3eaf.js"
+  },
+  {
+    "revision": "4ba63a3f8a03343816b9",
+    "url": "/luxbar2-experiment/static/js/main.4ba63a3f.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/luxbar2-experiment/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "4ba63a3f8a03343816b9",
+    "url": "/luxbar2-experiment/static/css/main.41bf0fbe.chunk.css"
+  },
+  {
+    "revision": "c454be53f9e8e2997c8a5e2435820fed",
+    "url": "/luxbar2-experiment/index.html"
+  }
+];
