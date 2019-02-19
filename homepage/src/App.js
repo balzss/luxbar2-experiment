ff688a10ea40ee9453bea2ba8a784aaee70b040d
balazs.saros <balazs.saros@gmail.com>
2019-02-19 15:32:02 +0100

    demo page additions

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -16,8 +16,7 @@ class App extends React.Component {
         return (
             <div className="App">
 
-                <nav className="luxbar luxbar-shadow1" style={{position: this.state.fixed ? 'fixed' : 'relative'}}>
-                    <a className="luxbar-brand" href="/">Luxbar Brand</a>
+                <nav className={ 'luxbar luxbar-shadow1' + (this.state.fixed ? ' luxbar-fixed' : '') }>
                     <input type="checkbox" id="luxbar-checkbox"/>
                     <label className="luxbar-hamburger luxbar-hamburger-doublespin" 
                         htmlFor="luxbar-checkbox"> <span></span> </label>
@@ -27,10 +26,16 @@ class App extends React.Component {
                         <li><a href="/">Item 3</a></li>
                         <li><a href="/">Item 4</a></li>
                         <li><a href="/">Item 5</a></li>
+                        <li><a href="/">Item 6</a></li>
                     </ul>
                 </nav>
 
                 <input type="checkbox" checked={this.state.fixed} onChange={this.changeFixed} style={{marginTop: this.state.fixed ? '88px' : '32px'}}/> Fixed to top: {this.state.fixed ? 'Yes' : 'No'}
+                <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
+                <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
+                <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
+                <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
+                <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
                 <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
                 <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
                 <p> Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium  Dolor tempora ut quisquam ab dolore Explicabo voluptates nihil temporibus tempora laboriosam. Ipsa eos neque ratione cumque deserunt? Deleniti repudiandae quas consequatur excepturi velit eligendi reiciendis, dolores Sit quod praesentium </p>
